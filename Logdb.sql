edd408e47d269ab39dba6ce26116a342247c018c
Narendra Kumar <narendra.kumar.becomp@gmail.com>
2019-10-13 22:15:58 +0200

    Add files via upload

---
+++ Logdb.sql
@@ -0,0 +1,30 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+
+GO
+
+CREATE TABLE [Log] (
+    [Id] bigint NOT NULL IDENTITY,
+    [Message] nvarchar(max) NULL,
+    [MessageTemplate] nvarchar(max) NULL,
+    [Level] nvarchar(128) NULL,
+    [TimeStamp] datetimeoffset NOT NULL,
+    [Exception] nvarchar(max) NULL,
+    [LogEvent] nvarchar(max) NULL,
+    [Properties] nvarchar(max) NULL,
+    CONSTRAINT [PK_Log] PRIMARY KEY ([Id])
+);
+
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20190816182132_LoggingDbInit', N'2.2.3-servicing-35854');
+
+GO
+
